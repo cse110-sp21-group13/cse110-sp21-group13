7861d06317050d4f32bc7b6548076c7256b4704d
dino-inc <ethan8960@gmail.com>
2021-05-07 10:29:08 -0700

    Added thrown error on missing field and success response

--- source/routes/create/bullet.js
+++ source/routes/create/bullet.js
@@ -7,6 +7,14 @@ module.exports = {
     '/create/bullet': {
         methods: ['post'],
         fn: function(req, res, next) {
+            // Check if every field exists, if not, create it (maybe reject it)
+            let requiredFields = ["user", "type", "signifier", "bulletType", "content", "date"];
+            requiredFields.forEach((jsonField, index)=>{
+                if(!req.body[jsonField]){
+                    console.log("Missing field "+jsonField+", rejecting request.");
+                    throw new Error('MISSING FIELD');
+                }
+            });
             db.post({
                 user: req.body.user,
                 type: req.body.type,
@@ -15,12 +23,14 @@ module.exports = {
                 content: req.body.content,
                 date: req.body.date
             })
-            .then((result) => {console.log(result)})
+            .then((result) => {
+                console.log(result);
+                res.send('success');
+            })
             .catch((err) => {
                 console.log(err);
                 res.send("error");
             });
-            res.send('success');
         }
     }
 }
