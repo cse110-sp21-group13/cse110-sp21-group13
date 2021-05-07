13494bd985fd0c663db425ae0d1018b4ab09619b
dino-inc <ethan8960@gmail.com>
2021-05-07 00:36:33 -0700

    Created bullet delete

---
+++ source/routes/delete/bullet.js
@@ -0,0 +1,21 @@
+const PouchDB = require('pouchdb');
+PouchDB.plugin(require('pouchdb-find'));
+const db = new PouchDB('bullet-journal');
+
+// Send in a valid json containing just the id and it will be expunged from the database
+module.exports = {
+    '/delete/bullet': {
+        methods: ['delete'],
+        fn: function(req, res, next) {
+            db.get(req.body.id)
+            .then((response) => {
+                db.remove(response._id, response._rev);
+                res.send("success");
+            })
+            .catch((err) => {
+                console.log(err);
+                res.send("error");
+            });
+        }
+    }
+}
