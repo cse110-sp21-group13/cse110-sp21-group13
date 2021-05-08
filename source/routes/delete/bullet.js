7cb5d1b97669beeb71973d00a40709284d987b07
dino-inc <ethan8960@gmail.com>
2021-05-07 17:05:12 -0700

    Change database name to db

--- source/routes/delete/bullet.js
+++ source/routes/delete/bullet.js
@@ -1,6 +1,6 @@
 const PouchDB = require('pouchdb');
 PouchDB.plugin(require('pouchdb-find'));
-const db = new PouchDB('bullet-journal');
+const db = new PouchDB('db');
 
 // Send in a valid json containing just the id and it will be expunged from the database
 module.exports = {
