693f1c47e97b74a6cc4184dd6b2f170fc5d15054
Ethan <30489196+dino-inc@users.noreply.github.com>
2021-05-10 13:40:44 -0700

    Apply suggestions from automatic style guideline suggestions
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>

--- source/routes/update/bullet.js
+++ source/routes/update/bullet.js
@@ -1,5 +1,5 @@
 const PouchDB = require('pouchdb');
-PouchDB.plugin(require('pouchdb-find'));
+PouchDB.plugin(require("pouchdb-find"));
 const db = new PouchDB('db');
 
 // Update a bullet's data
@@ -7,7 +7,7 @@ const db = new PouchDB('db');
 // {"id": "documentID", "updateField": {"customFieldToUpdate": "customDataToUpdate"}}
 module.exports = {
     "/update/bullet": {
-        methods: ['post'],
+        methods: ["post"],
         fn: function(req, res, next) {
             if(!req.body.updateField){
                 throw new Error("MISSING UPDATE DATA");
@@ -26,7 +26,7 @@ module.exports = {
                 console.log(req.body.updateField);
                 for(const updatedField in req.body.updateField){
                     jsonDoc[updatedField] = req.body.updateField[updatedField];
-                };
+                }
                 console.log(jsonDoc);
                 
                 // Put newly updated document into the databse
