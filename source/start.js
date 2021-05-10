d0125c929234ebfdf0a8e7d263b226de596dcb3a
Brandon Chaing <bchaing@ucsd.edu>
2021-05-10 03:08:04 -0700

    Formatted start.js file to abide by style guide

--- source/start.js
+++ source/start.js
@@ -1,20 +1,20 @@
-const express = require('express'),
-      routescan = require('express-routescan'),
-      session = require('express-session'),
-      bodyParser = require('body-parser'),
-      path = require('path');
+const express = require('express');
+const routescan = require('express-routescan');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(session({
   secret: 'anoshNSOEUnthoeuntTeNohesnas',
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: false, maxAge: Number(100000000) }
+  cookie: {secure: false, maxAge: Number(100000000)},
 }));
 
-//Globals
+// Globals
 
 global._base = __dirname + '/';
 global._env = app.get('env');
@@ -34,15 +34,15 @@ console.critical = function(message) {
 };
 
 routescan(app, {
-  ignoreInvalid: true
+  ignoreInvalid: true,
 });
 app.use('/', express.static(path.join(_base, 'docs')));
 app.use('/routes', express.static('routes'));
 
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   console.debug('Error encountered: ' + err.message);
   console.error(err);
-  res.json({ error: err.message });
+  res.json({error: err.message});
 });
 
 // HTTPS (don't have cert yet)
