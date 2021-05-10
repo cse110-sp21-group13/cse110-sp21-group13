d3f8c50c8a17cb63d73bce904ac6ae16b35ce4bd
Ethan <30489196+dino-inc@users.noreply.github.com>
2021-05-10 13:40:44 -0700

    Apply suggestions from automatic style guideline suggestions
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>

--- source/routes/delete/bullet.js
+++ source/routes/delete/bullet.js
@@ -1,10 +1,10 @@
 const PouchDB = require('pouchdb');
-PouchDB.plugin(require('pouchdb-find'));
+PouchDB.plugin(require("pouchdb-find"));
 const db = new PouchDB('db');
 
 // Send in a valid json containing just the id and it will be expunged from the database
 module.exports = {
-    '/delete/bullet': {
+    "/delete/bullet": {
         methods: ['delete'],
         fn: function(req, res, next) {
             db.get(req.body.id)
