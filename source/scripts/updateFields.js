af80c5850f7636cfebe96bcd2fddfdea94175a95
dino-inc <ethan8960@gmail.com>
2021-05-11 01:02:45 -0700

    Added JSDoc comment to conform to the linter

--- source/scripts/updateFields.js
+++ source/scripts/updateFields.js
@@ -2,30 +2,34 @@ const PouchDB = require('pouchdb');
 PouchDB.plugin(require('pouchdb-find'));
 const db = new PouchDB('db');
 
-
+/**
+ * Updates the fields of a PouchdDB document
+ * @param {response} res The response to send back with
+ * @param {request} req The request sent to the document
+ */
 function updateFields(res, req) {
-    db.get(req.body._id)
-        .then((response) => {
-            for (const updatedField in req.body.updateField) {
-            if (updatedField in response) {
-                response[updatedField] = req.body.updateField[updatedField];
-            } else {
-                throw new Error('INVALID FIELD SPECIFIED');
-            }
-            }
-            // Put newly updated document into the databse
-            db.put(response)
-                .then(() => {
-                res.send('success');
-                })
-                .catch((err) => {
-                console.log(err);
-                res.send('error');
-                });
-        })
-        .catch((err) => {
-            console.log(err);
-            res.send('error');
-        });
+  db.get(req.body._id)
+      .then((response) => {
+        for (const updatedField in req.body.updateField) {
+          if (updatedField in response) {
+            response[updatedField] = req.body.updateField[updatedField];
+          } else {
+            throw new Error('INVALID FIELD SPECIFIED');
+          }
+        }
+        // Put newly updated document into the databse
+        db.put(response)
+            .then(() => {
+              res.send('success');
+            })
+            .catch((err) => {
+              console.log(err);
+              res.send('error');
+            });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.send('error');
+      });
 }
-module.exports={updateFields}
+module.exports={updateFields};
