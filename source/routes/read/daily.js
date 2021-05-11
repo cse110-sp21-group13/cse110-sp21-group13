3962ad49f79dabf6c7f4b341991379967fa480c1
Zhongqian <zhl024@ucsd.edu>
2021-05-11 14:26:03 -0500

    update on read/daily for delete corresponding bullet

--- source/routes/read/daily.js
+++ source/routes/read/daily.js
@@ -6,40 +6,47 @@ const db = new PouchDB('db');
 module.exports = {
     '/read/daily': {
         methods: ['get'],
-        fn: function(req, res, next) {
+        fn: function (req, res, next) {
             db.get(req.body.id)
-            .then((response) => {
-                let lastBulletId = response.bullets[response.bullets.length-1];
-                for(let i in response.bullets){
-                    let id=response.bullets[i];
-                    if(!response.bullets[i] instanceof String) {
-                        continue;
+                .then((response) => {
+                    for (let i in response.bullets) {
+                        let lastBulletId = response.bullets[response.bullets.length - 1];
+                        console.log(i + " index of bullets ");
+                        let id = response.bullets[i];
+                        console.log(id);
+                        db.get(id)
+                            .then((bulletResponse) => {
+                                //replace id in "bullets" with the json object of that bullet
+                                response.bullets[i] = bulletResponse;
+                                console.log(lastBulletId + " equal to " + bulletResponse._id + " from " + bulletResponse);
+                                if (lastBulletId == bulletResponse._id) {
+                                    res.send(response);
+                                }
+                            })
+                            .catch((err) => {
+                                if(err){
+                                    response.bullets.splice(i, 1);
+                                    db.put(response)
+                                    .then(() => {})
+                                    .catch((err) => {
+                                        console.log(err);
+                                        console.log(3);
+                                    });
+                                    i--;
+                                    console.log(response.bullets.length + "equal to ");
+                                    if(id == lastBulletId){
+                                        res.send(response);
+                                    }
+                                }
+                                console.log(err);
+                                console.log("2");
+                            });
                     }
-                    //access the bullet document by its id
-                    db.get(id)
-                    .then((bulletResponse) => {
-                        if(bulletResponse){
-                            //replace id in "bullets" with the json object of that bullet
-                            response.bullets[i]= bulletResponse;
-                            console.log(lastBulletId +" equal to "+ bulletResponse._id + " from "+ bulletResponse);
-                            if(lastBulletId == bulletResponse._id){
-                                res.send(response);
-                            }
-                        }
-                        else{
-                            response.bullets.splice(i);
-                        }
-                    })
-                    .catch((err)=>{
-                        console.log(err);
-                        console.log("2");
-                    });
-                }
-            })
-            .catch((err) => {
-                console.log(err);
-                res.send("error");
-            });
+                })
+                .catch((err) => {
+                    console.log(err);
+                    res.send("error");
+                });
         }
     }
 }
