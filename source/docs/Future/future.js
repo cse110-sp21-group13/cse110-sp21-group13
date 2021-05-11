6dc101dde43be129ba4cec4fa32f9d26669feaf5
cnazemi <cameronsnazemi@gmail.com>
2021-05-11 15:29:28 -0700

    Updated future.js to fit requirements

--- source/docs/Future/future.js
+++ source/docs/Future/future.js
@@ -1,38 +1,39 @@
 // Create a 'close' button and append it to each list item
-let myNodelist = document.getElementsByTagName('LI');
+const myNodelist = document.getElementsByTagName('LI');
 for (let i = 0; i < myNodelist.length; i++) {
-  let span = document.createElement('SPAN');
-  let txt = document.createTextNode('\u00D7');
+  const span = document.createElement('SPAN');
+  const txt = document.createTextNode('\u00D7');
   span.className = 'close';
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
 }
 
 // Click on a close button to hide the current list item
-let close = document.getElementsByClassName('close');
+const close = document.getElementsByClassName('close');
 for (let i = 0; i < close.length; i++) {
   close[i].onclick = function() {
-    let div = this.parentElement;
+    const div = this.parentElement;
     div.style.display = 'none';
-  }
+  };
 }
 
 // Add a 'checked' symbol when clicking on a list item
-let list = document.querySelector('ul');
+const list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle('checked');
   }
 }, false);
 
-// Create a new list item when clicking on the 'Add' button
-/* *
- * Creates new row.
-*/
+/**
+ * Create a new list item when clicking on the 'Add' button
+ * @param {none}
+ * @returns {none}
+ */
 function newElement() {
-  let li = document.createElement('li');
-  let inputValue = document.getElementById('myInput').value;
-  let t = document.createTextNode(inputValue);
+  const li = document.createElement('li');
+  const inputValue = document.getElementById('myInput').value;
+  const t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === '') {
     alert('You must write something!');
@@ -41,24 +42,25 @@ function newElement() {
   }
   document.getElementById('myInput').value = '';
 
-  let span = document.createElement('SPAN');
-  let txt = document.createTextNode('\u00D7');
+  const span = document.createElement('SPAN');
+  const txt = document.createTextNode('\u00D7');
   span.className = 'close';
   span.appendChild(txt);
   li.appendChild(span);
 
   for (let i = 0; i < close.length; i++) {
     close[i].onclick = function() {
-      let div = this.parentElement;
+      const div = this.parentElement;
       div.style.display = 'none';
-    }
+    };
   }
 }
 
-// Edit daily log when pressed
-/* *
- * Allows editing of rows.
-*/
+/**
+ * Edit future log when pressed
+ * @param {none}
+ * @returns {none}
+ */
 function editFuture() {
   const button = document.getElementsByClassName('editBtn')[0].textContent;
   if (button == 'Save') {
