7c6121e73b8371e5a45611ba5e2c0b2e63af7af6
Zhongqian <zhl024@ucsd.edu>
2021-05-11 12:44:02 -0500

    fix on read/daily

--- source/routes/read/daily.js
+++ source/routes/read/daily.js
@@ -9,35 +9,32 @@ module.exports = {
         fn: function(req, res, next) {
             db.get(req.body.id)
             .then((response) => {
-                for(var i in response.bullets){
+                let lastBulletId = response.bullets[response.bullets.length-1];
+                for(let i in response.bullets){
+                    let id=response.bullets[i];
                     if(!response.bullets[i] instanceof String) {
                         continue;
                     }
-                    db.get(response.bullets[i])
+                    //access the bullet document by its id
+                    db.get(id)
                     .then((bulletResponse) => {
-                        response.bullets[i]= bulletResponse;
-
-                        db.put(response)
-                        .then(()=>{})
-                        .catch((err)=>{
-                            console.log(err);
-                        });
+                        if(bulletResponse){
+                            //replace id in "bullets" with the json object of that bullet
+                            response.bullets[i]= bulletResponse;
+                            console.log(lastBulletId +" equal to "+ bulletResponse._id + " from "+ bulletResponse);
+                            if(lastBulletId == bulletResponse._id){
+                                res.send(response);
+                            }
+                        }
+                        else{
+                            response.bullets.splice(i);
+                        }
                     })
                     .catch((err)=>{
                         console.log(err);
+                        console.log("2");
                     });
-                    
-                };
-                /*response.bullets.forEach((bullet,index) => {
-                    db.get(bullet)
-                    .then((bulletResponse) => {
-                        bullet=bulletResponse;
-                    })
-                    .catch((err) => {
-                        console.log(err);
-                    });
-                });*/
-                res.send(response);
+                }
             })
             .catch((err) => {
                 console.log(err);
