77c2c37df0dbca175eec30635f1bf8ebe72a3eb7
Zhongqian Li <64935761+zhl024@users.noreply.github.com>
2021-05-13 12:14:13 -0500

    resolve inconsistency on reporting error

--- source/routes/delete/daily.js
+++ source/routes/delete/daily.js
@@ -21,7 +21,7 @@ module.exports = {
                     })
                     .catch((err) => {
                         console.log(err);
-                        res.send("error caused by cannot get bullet Json by id");
+                        res.send("error");
                     });
                 })
                 //delete daily entry
