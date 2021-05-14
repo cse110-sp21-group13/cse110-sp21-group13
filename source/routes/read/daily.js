0520298a905f208d15750f866d17ec75f27ffbde
Zhongqian Li <64935761+zhl024@users.noreply.github.com>
2021-05-14 10:19:39 -0700

    [STRY/BE] REST API: Daily Journal Entries (#52)
    * modified CRUD for daily entry based on Ethan's code
    
    * update on read
    
    * fix on read/daily
    
    * update on read/daily for delete corresponding bullet
    
    * fix on read/daily
    
    * update on read+delete+update of daily
    
    * fix on read/daily
    
    * update on create/daily
    
    * update on create/daily
    
    Co-authored-by: Ethan Reiter <ereiter@ucsd.edu>
    
    * Handle a edge case of no bullets
    
    * reorder the assigning of monthkey and bullets
    
    * fix inconsistency on reporting success
    
    * fix inconsistency on reporting error+success in update
    
    * resolve inconsistency on reporting error
    
    * Update daily.js
    
    * Update daily.js
    
    Co-authored-by: Ethan Reiter <ereiter@ucsd.edu>

---
+++ source/routes/read/daily.js
@@ -0,0 +1,53 @@
+const PouchDB = require('pouchdb');
+PouchDB.plugin(require('pouchdb-find'));
+const db = new PouchDB('db');
+/* *
+read daily will send response with form 
+{
+    "user": "dave",
+    "date": "2021-05-09",
+    "monthKey": "05-09",
+    "bullets": [
+        {bullet1 json objectt},
+        {bullet2 json object}
+    ]
+}
+*/
+module.exports = {
+    '/read/daily': {
+        methods: ['get'],
+        fn: function (req, res, next) {
+            let tempArr = [];
+            //get Daily entry documnet by id
+            db.get(req.body._id)
+            .then(response => {
+                if(response.bullets.length == 0){
+                    res.send(response);
+                }
+                let curr = 0;
+                //get into the bullets array inside daily entry
+                response.bullets.forEach((bullet, index, array) => {
+                    //get bullet document by id
+                    db.get(bullet)
+                    .then((bulletResponse) => {
+                        tempArr.push(bulletResponse);
+                    })
+                    .catch((err) => {
+                        res.send("error");
+                    })
+                    .finally(()=>{
+                        curr++; 
+                        if (curr >= array.length) {
+                            response.bullets=tempArr;
+                            res.send(response);
+                            return;
+                        }
+                    });
+                });
+            })
+            .catch((err) => {
+                res.send("error");
+            });
+        }
+    }
+}
