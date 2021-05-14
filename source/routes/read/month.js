f7c27a2aee157a26cae2ea8527247b0aaee06759
Zhongqian Li <64935761+zhl024@users.noreply.github.com>
2021-05-14 12:05:09 -0700

    [STRY/BE] REST API: Months (#54)
    * modified CRUD for daily entry based on Ethan's code
    
    * update on read
    
    * fix on read/daily
    
    * update on read/daily for delete corresponding bullet
    
    * fix on read/daily
    
    * update on read+delete+update of daily
    
    * REST API  for month
    
    * bad-branch
    
    * daily
    
    * month
    
    * Delete bullet.js
    
    * Delete daily.js
    
    * Delete bullet.js
    
    * Delete daily.js
    
    * Delete bullet.js
    
    * Delete daily.js
    
    * Delete daily_optimization.js
    
    * Delete bullet.js
    
    * Delete daily.js
    
    * Delete 051221-sprint1check.md
    
    * Update month.js
    
    * resolve inconsistency on report error
    
    * resolve inconsistency on report success
    
    * resolve inconsistency in reporting success
    
    * resolve inconsistency on reporting error
    
    * send error instead of print it
    
    * Update source/routes/create/month.js
    
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    
    * Update source/routes/delete/month.js
    
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    
    * Update source/routes/read/month.js
    
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    
    * Update source/routes/create/month.js
    
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    
    * Update source/routes/read/month.js
    
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    
    * Update source/routes/create/month.js
    
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    
    * Update source/routes/create/month.js
    
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    
    * Update source/routes/read/month.js
    
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    
    * Update source/routes/read/month.js
    
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    
    * Update source/routes/update/month.js
    
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    
    * Update source/routes/read/month.js
    
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    
    * Update source/routes/create/month.js
    
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    
    * Update source/routes/read/month.js
    
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    
    * Update source/routes/create/month.js
    
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    
    * Update source/routes/delete/month.js
    
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    
    * Update source/routes/create/month.js
    
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    
    * Update source/routes/delete/month.js
    
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    
    * pacakge update
    
    * Update source/routes/delete/month.js
    
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    
    * Applied the linter
    
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    Co-authored-by: dino-inc <ethan8960@gmail.com>

---
+++ source/routes/read/month.js
@@ -0,0 +1,95 @@
+const PouchDB = require('pouchdb');
+PouchDB.plugin(require('pouchdb-find'));
+const db = new PouchDB('db');
+/* *
+read month will send response with form
+{
+    "user": "dave",
+    "month": "May",
+    "bullets": [
+        {bullet1 json objectt},
+        {bullet2 json object}
+    ],
+    "daillys":[
+        {"date": "...", "daily": "..."}
+    ]
+}
+*/
+module.exports = {
+  '/read/month': {
+    methods: ['get'],
+    fn: function(req, res, next) {
+      const tempArr = [];
+
+      // create index for query
+      db.createIndex({
+        index: {
+          fields: ['user'],
+        },
+      }).then((result) => {
+        console.log(result);
+      }).catch((err) => {
+        res.send('error');
+      });
+
+      // get month page by id
+      db.get(req.body._id)
+          .then((response) => {
+            // handle edge case: nothing inside the bullets
+            if (response.bullets.length == 0) {
+              // grab all daily journal entries in that month
+              db.find({
+                selector: {
+                  user: response.user,
+                  monthKey: response.month,
+                  docType: 'dailyJournal',
+                },
+                fields: ['date', '_id'],
+              })
+                  .then((result) => {
+                    response.dailys = result;
+                    res.send(response);
+                  });
+            }
+
+            let curr = 0;
+            // get inside the bullets array
+            response.bullets.forEach((bullet, index, array) => {
+              // get bullet document by id
+              db.get(bullet)
+                  .then((bulletResponse) => {
+                    tempArr.push(bulletResponse);
+                  })
+                  .catch((err) => {
+                    res.send('error');
+                  })
+                  .finally(() => {
+                    // send out bullets and all daily journal entries
+                    curr++;
+                    if (curr >= array.length) {
+                      response.bullets = tempArr;
+                      db.find({
+                        selector: {
+                          user: response.user,
+                          monthKey: response.month,
+                          docType: 'dailyJournal',
+                        },
+                        fields: ['date', '_id'],
+                      })
+                          .then((result) => {
+                            response.dailys = result;
+                            // send out the response
+                            res.send(response);
+                          });
+                      return;
+                    }
+                  });
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+            res.send('error');
+          });
+    },
+  },
+};
