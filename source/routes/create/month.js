8c92b42c5fb19cdb51c8f580f940e9234111b1b1
dino-inc <ethan8960@gmail.com>
2021-05-14 12:02:44 -0700

    Applied the linter

--- source/routes/create/month.js
+++ source/routes/create/month.js
@@ -1,11 +1,11 @@
-const PouchDB = require("pouchdb");
-PouchDB.plugin(require("pouchdb-find"));
-const db = new PouchDB("db");
+const PouchDB = require('pouchdb');
+PouchDB.plugin(require('pouchdb-find'));
+const db = new PouchDB('db');
 
 /*
 Send in json form of the month page and it gets sent to the database
 
-The month page will be of form: 
+The month page will be of form:
 {
     "user": "dave",
     "month": "May",
@@ -13,32 +13,32 @@ The month page will be of form:
 }
 */
 module.exports = {
-    '/create/month': {
-        methods: ["post"],
-        fn: function(req, res, next) {
-            // Check if every field exists, if not, throw error
-            let requiredFields = ["user", "month", "bullets"];
-            requiredFields.forEach((jsonField, index) =>{
-                if(!req.body[jsonField]){
-                    throw new Error("MISSING FIELD");
-                }
-            });
-            db.post({
-                // Stores the user associated with the month page
-                user: req.body.user,
-                // Stores the month the month page was created
-                month: req.body.month,
-                // Stores the bullets id in an array
-                bullets: req.body.bullets,
-            })
-            .then((response) => {
-                console.log(response);
-                res.send(response);
-            })
-            .catch((err) => {
-                console.log(err);
-                res.send("error");
-            });
+  '/create/month': {
+    methods: ['post'],
+    fn: function(req, res, next) {
+      // Check if every field exists, if not, throw error
+      const requiredFields = ['user', 'month', 'bullets'];
+      requiredFields.forEach((jsonField, index) =>{
+        if (!req.body[jsonField]) {
+          throw new Error('MISSING FIELD');
         }
-    }
-}; 
+      });
+      db.post({
+        // Stores the user associated with the month page
+        user: req.body.user,
+        // Stores the month the month page was created
+        month: req.body.month,
+        // Stores the bullets id in an array
+        bullets: req.body.bullets,
+      })
+          .then((response) => {
+            console.log(response);
+            res.send(response);
+          })
+          .catch((err) => {
+            console.log(err);
+            res.send('error');
+          });
+    },
+  },
+};
--- source/routes/delete/month.js
+++ source/routes/delete/month.js
@@ -1,37 +1,37 @@
-const PouchDB = require("pouchdb");
+const PouchDB = require('pouchdb');
 PouchDB.plugin(require('pouchdb-find'));
-const db = new PouchDB("db");
+const db = new PouchDB('db');
 
 /* *
 Delete month page json object and all bullet documents in that month page.
 */
 module.exports = {
-    "/delete/month": {
-        methods: ["delete"],
-        fn: function(req, res, next) {
-            //get month page by id
-            db.get(req.body._id)
-            .then((response) => {
-                //access all bullet documents' id
-                response.bullets.forEach((bullet, index) => {
-                    db.get(bullet)
-                    .then(function(doc){
-                        //delete bullet document
-                        return db.remove(doc._id, doc._rev);
-                    })
-                    .catch((err) => {
-                        console.log(err);
-                        res.send("error");
-                    });
-                })
-                //delete month page
-                db.remove(response._id, response._rev);
-                res.send("success");
-            })
-            .catch((err) => {
-                console.log(err);
-                res.send("error");
+  '/delete/month': {
+    methods: ['delete'],
+    fn: function(req, res, next) {
+      // get month page by id
+      db.get(req.body._id)
+          .then((response) => {
+            // access all bullet documents' id
+            response.bullets.forEach((bullet, index) => {
+              db.get(bullet)
+                  .then(function(doc) {
+                    // delete bullet document
+                    return db.remove(doc._id, doc._rev);
+                  })
+                  .catch((err) => {
+                    console.log(err);
+                    res.send('error');
+                  });
             });
-        }
-    }
-}
+            // delete month page
+            db.remove(response._id, response._rev);
+            res.send('success');
+          })
+          .catch((err) => {
+            console.log(err);
+            res.send('error');
+          });
+    },
+  },
+};
