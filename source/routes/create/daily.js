7638448682ab967f985f285fa3fe5d332b0a52e8
Zhongqian Li <64935761+zhl024@users.noreply.github.com>
2021-05-14 12:00:07 -0700

    [STRY/BE] REST API: Daily Journal Entries (#56)
    * modified CRUD for daily entry based on Ethan's code
    
    * update on read
    
    * fix on read/daily
    
    * update on read/daily for delete corresponding bullet
    
    * fix on read/daily
    
    * update on read+delete+update of daily
    
    * fix on read/daily
    
    * update on create/daily
    
    * update on create/daily
    
    Co-authored-by: Ethan Reiter <ereiter@ucsd.edu>
    
    * Handle a edge case of no bullets
    
    * reorder the assigning of monthkey and bullets
    
    * fix inconsistency on reporting success
    
    * fix inconsistency on reporting error+success in update
    
    * resolve inconsistency on reporting error
    
    * Update daily.js
    
    * Update daily.js
    
    * update packages
    
    * Update source/routes/update/daily.js
    
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    
    * Update source/routes/create/daily.js
    
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    
    * Update source/routes/update/daily.js
    
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    
    * Update source/routes/read/daily.js
    
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    
    * Update source/routes/delete/daily.js
    
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    
    * Update source/routes/read/daily.js
    
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    
    * Update source/routes/create/daily.js
    
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    
    * Update source/routes/update/daily.js
    
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    
    * Update source/routes/delete/daily.js
    
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    
    * Update source/routes/create/daily.js
    
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    
    * Update source/routes/create/daily.js
    
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    
    * Update source/routes/create/daily.js
    
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    
    * Update source/routes/read/daily.js
    
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    
    * Applied the linter to the daily routes
    
    Co-authored-by: Ethan Reiter <ereiter@ucsd.edu>
    Co-authored-by: b2wu <81795401+b2wu@users.noreply.github.com>
    Co-authored-by: dino-inc <ethan8960@gmail.com>

---
+++ source/routes/create/daily.js
@@ -0,0 +1,49 @@
+const PouchDB = require('pouchdb');
+PouchDB.plugin(require('pouchdb-find'));
+const db = new PouchDB('db');
+
+/*
+Send in json form of the bullet and it gets sent to the database
+
+The daily entry db will be of form:
+{
+    "user": "dave",
+    "date": "2021-05-09",
+    "docType": "dailyJournal",
+    "monthKey": "May",
+    "bullets": ["bullet-id1", "bullet-id2", ...]
+}
+*/
+module.exports = {
+  '/create/daily': {
+    methods: ['post'],
+    fn: function(req, res, next) {
+      // Check if every field exists, if not, throw error
+      const requiredFields = ['user', 'date', 'docType', 'monthKey', 'bullets'];
+      requiredFields.forEach((jsonField, index) =>{
+        if (!req.body[jsonField]) {
+          throw new Error('MISSING FIELD');
+        }
+      });
+      db.post({
+        // Stores the user associated with the daily entry
+        user: req.body.user,
+        // Stores the date the daily entry was created
+        date: req.body.date,
+        // Stores the docType of the daily entry
+        docType: req.body.docType,
+        // store a corresponding monthly-key
+        monthKey: req.body.monthKey,
+        // Stores the bullets in an array
+        bullets: req.body.bullet,
+      })
+          .then((response) => {
+            res.send(response);
+          })
+          .catch((err) => {
+            console.log(err);
+            res.send('error');
+          });
+    },
+  },
+};
