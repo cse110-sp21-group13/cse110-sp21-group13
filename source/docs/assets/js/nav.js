c2e30e6e0f6d983c3cba11cbd54cd942f5a9b226
Lars Vlahakis <lvlahaki@ucsd.edu>
2021-05-21 15:04:57 -0700

    styling, animations, changed approach to limit js

--- source/docs/assets/js/nav.js
+++ source/docs/assets/js/nav.js
@@ -3,28 +3,26 @@ const navHtml = document.createElement('div');
 navHtml.class = 'navbar';
 navHtml.innerHTML = 
     `<link rel="stylesheet" href="assets/css/nav.css">
-    <div id="topbar">
-      <img id="hamburger" src="assets/images/hamburger.svg" alt="three horizonal lines" width="50px">
-      <img id="logo" src="assets/images/13bullet_logo.png" alt="a smiling black cat" width="75px">
-      <img id="bullet" src="assets/images/13ullet.svg" alt="13Bullet" width="170px"></img>
+    <div class="topbar">
+      <label for="hamburger">
+        <img src="assets/images/hamburger.svg" alt="three horizonal lines" width="50px">
+      </label>
+      <img id="barlogo" src="assets/images/13bullet_logo.png" alt="a smiling black cat" width="75px">
+      <img id="bartitle" src="assets/images/13ullet.svg" alt="13Bullet" width="170px"></img>
+      <h2 id="greeting">Hello!</h2>
     </div>
     <nav>
-      <a href="daily.html">Daily Log</a>
-      <a href="calendar.html">Montly Log</a>
-      <button>sign out</button>
+      <input type="checkbox" id="hamburger"/>
+      <div id="hamitems">
+        <a href="daily.html">Daily Log ∨</a>
+        <a href="calendar.html">Monthly Log ∨</a>
+        <button>SIGN OUT</button>
+      </div>
     </nav>`;
     
 header.appendChild(navHtml);
-
-const hamburger = document.getElementById('hamburger');
-const navItems = document.getElementsByTagName('nav')[0];
-let vis = false;
-hamburger.addEventListener('click', () => {
-  if (!vis){
-      navItems.style.display = 'block';
-      vis = true;
-  } else {
-      navItems.style.display = 'none';
-      vis = false;
-  }
-});
+/*
+const greeting = document.getElementById("greeting"){
+  
+}
+*/
