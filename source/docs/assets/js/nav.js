c6932ca071e50b51b8dbe15f1b52f6d27e0e3f6a
Lars Vlahakis <lvlahaki@ucsd.edu>
2021-05-22 18:17:32 -0700

    style changes, switched to iframe implementation

--- source/docs/assets/js/nav.js
+++ source/docs/assets/js/nav.js
@@ -1,28 +1,8 @@
-const header = document.getElementsByTagName('header')[0];
-const navHtml = document.createElement('div');
-navHtml.class = 'navbar';
-navHtml.innerHTML = 
-    `<link rel="stylesheet" href="assets/css/nav.css">
-    <div class="topbar">
-      <label for="hamburger">
-        <img src="assets/images/hamburger.svg" alt="three horizonal lines" width="50px">
-      </label>
-      <img id="barlogo" src="assets/images/13bullet_logo.png" alt="a smiling black cat" width="75px">
-      <img id="bartitle" src="assets/images/13ullet.svg" alt="13Bullet" width="170px"></img>
-      <h2 id="greeting">Hello!</h2>
-    </div>
-    <nav>
-      <input type="checkbox" id="hamburger"/>
-      <div id="hamitems">
-        <a href="daily.html">Daily Log ∨</a>
-        <a href="calendar.html">Monthly Log ∨</a>
-        <button>SIGN OUT</button>
-      </div>
-    </nav>`;
-    
-header.appendChild(navHtml);
-/*
-const greeting = document.getElementById("greeting"){
-  
-}
-*/
+fetch('../read/user', {
+  method: 'GET',
+})
+    .then((response) => response.json())
+    .then(data => {
+        const greeting = document.getElementById('greeting');
+        greeting.innerText = 'Hello ' + data.username + '!';
+    })
