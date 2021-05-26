16587a2ea6bd094ffec598f43af84395b3093cd2
Zhongqian <zhl024@ucsd.edu>
2021-05-26 16:38:04 -0500

    fully functional tests for user + session

--- source/__tests__/user.test.js
+++ source/__tests__/user.test.js
@@ -8,14 +8,16 @@ var app = require('../start');
 var request = require('supertest');
 var authenticatedUser = request.agent(app);
 
-var firstUser = {
-    'username': 'non9',
-    'password': '1101103232'
-};
 describe("User REST API Unit Test", function(){
     var user;
+    var firstUser = {
+        'username': 'non9',
+        'password': '2233'
+    };
     user = firstUser.username;
-    /*it('Test 2: create a valid user', function(done) {
+    //create route of user functions correctly, but since it requires 
+    //new username each time, I comment it out.
+    it('Test 1: create a valid user', function(done) {
         chai.request(baseUrl)
         .post('create/user')
         .set('Content-Type', 'application/json')
@@ -26,10 +28,10 @@ describe("User REST API Unit Test", function(){
             user = firstUser.username;
             done();
         });
-    });*/
+    });
 
     // log in/ create the session
-    it('Test 3: create a session for the user', function(done){
+    it('Test 2: create a session for the user', function(done){
         authenticatedUser
         .post('/create/session')
         .set('Content-Type', 'application/json')
@@ -39,17 +41,67 @@ describe("User REST API Unit Test", function(){
             done();
         });
     });
-    it('Test 4: get a user', function (done) {
+
+    //Get the user of current session
+    it('Test 3: get the current user', function (done) {
         authenticatedUser
         .get('/read/user')
         .set('Content-Type', 'application/json')
         .end(function(err, res){
-            //console.log(res.body);
-            //console.log(res.body);
             expect(res.body.username).to.equal(user);
             expect(res.body.docType).to.equal('user');
             done();
         });
-    })
+    });
+
+    var updateUser = {
+        "username": "non9",
+        "updateField": {
+            "password": "2233"
+        }
+    }
+    it('Test 4: update the current user', function(done){
+        authenticatedUser
+        .post('/update/user')
+        .set('Content-Type', 'application/json')
+        .send(updateUser)
+        .end(function(err,res){
+            //console.log(res.text);
+            expect(res.text).to.equal("success");
+            done();
+        });
+    });
+
+    it('Test 5: delete current session', function(done){
+        authenticatedUser
+        .delete('/delete/session')
+        .set('Content-Type', 'application/json')
+        .end(function(err,res){
+            expect(res.body.result).to.equal("Success");
+            done();
+        });
+    });
+
+    it('Test 6: Recreate a session for current user', function(done){
+        authenticatedUser
+        .post('/create/session')
+        .set('Content-Type', 'application/json')
+        .send(firstUser)
+        .end(function(err,res){
+            expect(res.statusCode).to.equal(302);
+            done();
+        });
+    });
+
+    it('Test 7: delete current user', function(done){
+        authenticatedUser
+        .delete('/delete/user')
+        .set('Content-Type', 'application/json')
+        .end(function(err,res){
+            expect(res.text).to.equal("success");
+            done();
+        });
+    });
+
 
 })
