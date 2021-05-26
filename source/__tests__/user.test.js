edcf077eaf9ca73e42e9906a61d81a5b43798d7f
Zhongqian <zhl024@ucsd.edu>
2021-05-26 15:47:20 -0500

    start session successfully

--- source/__tests__/user.test.js
+++ source/__tests__/user.test.js
@@ -4,21 +4,18 @@ const expect = chai.expect;
 const baseUrl = "localhost:3001/";
 chai.use(chaiHttp);
 
+var app = require('../start');
+var request = require('supertest');
+var authenticatedUser = request.agent(app);
+
+var firstUser = {
+    'username': 'non9',
+    'password': '1101103232'
+};
 describe("User REST API Unit Test", function(){
-    it('Test1: server is live', function(done) {
-        chai.request(baseUrl)
-        .get('/')
-        .end(function(err, res){
-            expect(res).to.have.status(200);
-            done();
-        });
-    })
     var user;
-    var firstUser = {
-        'username': 'non9',
-        'password': '1101103232'
-    };
-    it('Test 2: create a valid user', function(done) {
+    user = firstUser.username;
+    /*it('Test 2: create a valid user', function(done) {
         chai.request(baseUrl)
         .post('create/user')
         .set('Content-Type', 'application/json')
@@ -29,23 +26,25 @@ describe("User REST API Unit Test", function(){
             user = firstUser.username;
             done();
         });
-    });
+    });*/
+
+    // log in/ create the session
     it('Test 3: create a session for the user', function(done){
-        chai.request(baseUrl)
-        .post('create/session')
+        authenticatedUser
+        .post('/create/session')
         .set('Content-Type', 'application/json')
         .send(firstUser)
-        .end(function (err, res) {
-            expect(res).to.have.status(200);
-            expect(res.body.result).to.equal('Success');
+        .end(function(err,res){
+            expect(res.statusCode).to.equal(302);
             done();
         });
-    })
+    });
     it('Test 4: get a user', function (done) {
-        chai.request(baseUrl)
-        .get('read/user')
+        authenticatedUser
+        .get('/read/user')
         .set('Content-Type', 'application/json')
         .end(function(err, res){
+            //console.log(res.body);
             //console.log(res.body);
             expect(res.body.username).to.equal(user);
             expect(res.body.docType).to.equal('user');
