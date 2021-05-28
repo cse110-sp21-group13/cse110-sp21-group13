b9cd4365fe8af9dbc0d96adc5525f8a7f4a97d63
Zhongqian <zhl024@ucsd.edu>
2021-05-28 15:07:07 -0500

    add an error handler

--- source/routes/update/user.js
+++ source/routes/update/user.js
@@ -39,7 +39,11 @@ module.exports = {
                         res.send('error');
                       });
                 });
-          });
+          })
+          .catch((err) =>{
+            console.log(err);
+            res.send('error');
+          })
     },
   },
 };
