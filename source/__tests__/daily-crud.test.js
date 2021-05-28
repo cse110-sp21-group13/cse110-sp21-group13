3537331ee6f717d09315258c850a14dde75494c6
cnazemi <cameronsnazemi@gmail.com>
2021-05-27 21:15:30 -0700

    Added daily crud test file

---
+++ source/__tests__/daily-crud.test.js
@@ -0,0 +1,165 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+const baseUrl = 'localhost:3001/';
+chai.use(chaiHttp);
+
+const app = require('../start');
+const request = require('supertest');
+const authenticatedUser = request.agent(app);
+
+describe('User REST API Unit Test', function() {
+  let user;
+  const firstUser = {
+    'username': 'cameron2',
+    'password': '123456',
+  };
+  user = firstUser.username;
+
+  const newDay = {
+    'day': '20',
+    'month': '2021-5',
+    'bullets': [],
+  };
+
+  // log in/ create the session
+  it('Test 2: create a session for the user', function(done) {
+    authenticatedUser
+        .post('/create/session')
+        .set('Content-Type', 'application/json')
+        .send(firstUser)
+        .end(function(err, res) {
+          expect(res.statusCode).to.equal(302);
+          expect(res.text).to.equal('Found. Redirecting to session/success');
+          done();
+        });
+  });
+
+  // new daily creation 
+  it('Test 3: create a daily', function(done) {
+    authenticatedUser
+        .post('/create/daily')
+        .set('Content-Type', 'application/json')
+        .send(newDay)
+        .end(function(err, res) {
+          expect(res.statusCode).to.equal(200);
+          expect(res.ok).to.equal(true);
+          done();
+        });
+  });
+
+
+  let delJSON = {
+      '_id': ""
+  };
+
+
+  // get daily
+  it('Test 4: get the day', function(done) {
+    authenticatedUser
+        .get('/read/daily/' + newDay.month + '/' + newDay.day)
+        .set('Content-Type', 'application/json')
+        .end(function(err, res) {
+          delJSON._id = res.body._id;
+          expect(res.body.user).to.equal(user);
+          expect(res.body.docType).to.equal('daily');
+          done();
+        });
+  });
+
+  
+  // delete daily
+  it('Test 5: delete daily', function(done) {
+    authenticatedUser
+        .delete('/delete/daily')
+        .set('Content-Type', 'application/json')
+        .send(delJSON)
+        .end(function(err, res) {
+          expect(res.text).to.equal('success');
+          done();
+        });
+  });
+
+  // attempt to delete same daily should fail and reponse should be error
+  it('Test 6: delete same daily again', function(done) {
+    authenticatedUser
+        .delete('/delete/daily')
+        .set('Content-Type', 'application/json')
+        .send(delJSON)
+        .end(function(err, res) {
+          expect(res.text).to.equal('error');
+          done();
+        });
+  });
+
+
+  const updateDailyJSON = {
+    
+    "_id": "",
+    "updateField": {
+        "day": "10",
+        "month": "2021-12",
+        "bullets": []
+    }
+    
+  };
+
+  // new daily creation 
+  it('Test 7: create a daily', function(done) {
+    authenticatedUser
+        .post('/create/daily')
+        .set('Content-Type', 'application/json')
+        .send(newDay)
+        .end(function(err, res) {
+          updateDailyJSON._id = res.body._id;
+          expect(res.statusCode).to.equal(200);
+          expect(res.ok).to.equal(true);
+          done();
+        });
+  });
+
+  // get new daily
+  it('Test 4: get the day', function(done) {
+    authenticatedUser
+        .get('/read/daily/' + newDay.month + '/' + newDay.day)
+        .set('Content-Type', 'application/json')
+        .end(function(err, res) {
+          expect(res.body.month).to.equal('2021-5');
+          expect(res.body.day).to.equal('20');
+          expect(res.body.user).to.equal(user);
+          expect(res.body.docType).to.equal('daily');
+          done();
+        });
+  });
+
+  
+  // update daily
+  it('Test 8: update daily', function(done) {
+    authenticatedUser
+        .post('/update/daily')
+        .set('Content-Type', 'application/json')
+        .send(updateDailyJSON)
+        .end(function(err, res) {
+          expect(res.text).to.equal('success');
+          done();
+        });
+  });
+
+
+  // get updated daily
+  it('Test 9: get the new day', function(done) {
+    authenticatedUser
+        .get('/read/daily/' + updateDailyJSON.updateField.month + '/' + updateDailyJSON.updateField.day)
+        .set('Content-Type', 'application/json')
+        .end(function(err, res) {
+          expect(res.body.month).to.equal('2021-12');
+          expect(res.body.day).to.equal('10');
+          expect(res.body.user).to.equal(user);
+          expect(res.body.docType).to.equal('daily');
+          done();
+        });
+  });
+  
+  
+
+});
