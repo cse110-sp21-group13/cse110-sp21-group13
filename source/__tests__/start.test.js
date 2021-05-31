f23ef1df18a0b45c3c76f418276d09f907896431
Brandon Chaing <bchaing@ucsd.edu>
2021-05-31 10:07:53 -0700

    Update to destroy db

--- source/__tests__/start.test.js
+++ source/__tests__/start.test.js
@@ -1,6 +1,10 @@
 const request = require('supertest');
 const {app, server} = require('../../source/start');
 
+const PouchDB = require('pouchdb');
+PouchDB.plugin(require('pouchdb-find'));
+const db = new PouchDB('db');
+
 describe('Test the root path', () => {
   test('It should response the GET method', (done) => {
     request(app)
@@ -11,7 +15,8 @@ describe('Test the root path', () => {
         });
   });
 
-  afterAll((done) => {
+  afterAll(async (done) => {
     server.close();
+    await db.destroy();
   });
 });
