680d787948eb0f9988a975dcfa33d747d56da480
Zhongqian <zhl024@ucsd.edu>
2021-05-31 19:57:18 -0500

    Add more test to test more thoroughly

--- source/__tests__/user.test.js
+++ source/__tests__/user.test.js
@@ -45,8 +45,19 @@ describe('User REST API Unit Test', function() {
         });
   });
 
+  it('Test 3: check whether create session successfully', function(done) {
+    authenticatedUser
+        .get('/create/session/success')
+        .set('Content-Type', 'application/json')
+        .end(function(err, res) {
+          expect(res.body.result).to.equal('Success');
+          done();
+        });
+  });
+
+
   // Get the user of current session
-  it('Test 3: get the current user', function(done) {
+  it('Test 4: get the current user', function(done) {
     authenticatedUser
         .get('/read/user')
         .set('Content-Type', 'application/json')
@@ -64,7 +75,7 @@ describe('User REST API Unit Test', function() {
       'newPassword': '1234',
     },
   };
-  it('Test 4: update the current user password', function(done) {
+  it('Test 5: update the current user password', function(done) {
     authenticatedUser
         .post('/update/user')
         .set('Content-Type', 'application/json')
@@ -75,7 +86,7 @@ describe('User REST API Unit Test', function() {
         });
   });
 
-  it('Test 5: delete current session', function(done) {
+  it('Test 6: delete current session', function(done) {
     authenticatedUser
         .delete('/delete/session')
         .set('Content-Type', 'application/json')
@@ -85,13 +96,37 @@ describe('User REST API Unit Test', function() {
         });
   });
 
+  it('Test 7: create session for invalid user',
+      function(done) {
+        authenticatedUser
+            .post('/create/session')
+            .set('Content-Type', 'application/json')
+            .send(firstUser)
+            .end(function(err, res) {
+              expect(res.text)
+                  .to.equal('Found. Redirecting to session/fail');
+              done();
+            });
+      });
+
+  it('Test 8: check whether fail at creating session', function(done) {
+    authenticatedUser
+        .get('/create/session/fail')
+        .set('Content-Type', 'application/json')
+        .end(function(err, res) {
+          expect(res.body.error).to.equal('Incorrect credentials');
+          done();
+        });
+  });
+
+
   const tempUser = {
     'username': 'non9',
     'password': '1234',
   };
 
   // if success, the password changed successfully
-  it('Test 6: Recreate a session for current user with updated password',
+  it('Test 9: Recreate a session for current user with updated password',
       function(done) {
         authenticatedUser
             .post('/create/session')
@@ -108,13 +143,14 @@ describe('User REST API Unit Test', function() {
   const tempUpdateUser = {
     'username': 'non9',
     'updateField': {
+      'customFieldToUpdate': 'light',
       'oldPassword': '1234',
       'newPassword': '2233',
     },
   };
 
   // so that next time run test 2 will not cause error due to wrong password
-  it('Test 7: reupdate password back to original', function(done) {
+  it('Test 10: reupdate password back to original', function(done) {
     authenticatedUser
         .post('/update/user')
         .set('Content-Type', 'application/json')
@@ -124,6 +160,30 @@ describe('User REST API Unit Test', function() {
           done();
         });
   });
+  it('Test 11: get the current user', function(done) {
+    authenticatedUser
+        .get('/read/user')
+        .set('Content-Type', 'application/json')
+        .end(function(err, res) {
+          expect(res.body.username).to.equal(user);
+          expect(res.body.docType).to.equal('user');
+          expect(res.body.style).
+              to.equal(tempUpdateUser.updateField.customFieldToUpdate);
+          done();
+        });
+  });
+  it('Test 12: error when try to update with wrong password', function(done) {
+    authenticatedUser
+        .post('/update/user')
+        .set('Content-Type', 'application/json')
+        .send(tempUpdateUser)
+        .end(function(err, res) {
+          expect(res.text).to.equal('error: Old password doesn\'t match ' +
+                          'existing password');
+          done();
+        });
+  });
+
 
   const newDay = {
     'day': '20',
@@ -133,7 +193,7 @@ describe('User REST API Unit Test', function() {
 
   // create a daily for that user to test whether delete/user
   // will delete this daily too
-  it('Test 8: create a daily', function(done) {
+  it('Test 13: create a daily', function(done) {
     authenticatedUser
         .post('/create/daily')
         .set('Content-Type', 'application/json')
@@ -145,8 +205,21 @@ describe('User REST API Unit Test', function() {
         });
   });
 
+  it('Test 14: read current user data', function(done) {
+    authenticatedUser
+        .get('/read/user-data')
+        .set('Content-Type', 'application/json')
+        .end(function(err, res) {
+          const data = JSON.parse(res.text);
+          expect(data.docs[0].user).to.equal(user);
+          expect(data.docs[0].day).to.equal(newDay.day);
+          expect(data.docs[0].month).to.equal(newDay.month);
+          done();
+        });
+  });
+
   // so that next time run test 1 will not cause error due to duplicate username
-  it('Test 9: delete current user', function(done) {
+  it('Test 15: delete current user', function(done) {
     authenticatedUser
         .delete('/delete/user-data')
         .set('Content-Type', 'application/json')
@@ -156,7 +229,7 @@ describe('User REST API Unit Test', function() {
         });
   });
 
-  it('Test 10: error when the daily does not exist', function(done) {
+  it('Test 16: error when the daily does not exist', function(done) {
     authenticatedUser
         .get('/read/daily/' + newDay.month + '/' + newDay.day)
         .set('Content-Type', 'application/json')
@@ -166,7 +239,7 @@ describe('User REST API Unit Test', function() {
         });
   });
 
-  it('Test 11: error if read invalid user', function(done) {
+  it('Test 17: error if read invalid user', function(done) {
     authenticatedUser
         .get('/read/user')
         .set('Content-Type', 'application/json')
@@ -176,7 +249,7 @@ describe('User REST API Unit Test', function() {
         });
   });
 
-  it('Test 12: error if delete invalid user', function(done) {
+  it('Test 18: error if delete invalid user', function(done) {
     authenticatedUser
         .delete('/delete/user-data')
         .set('Content-Type', 'application/json')
@@ -189,7 +262,7 @@ describe('User REST API Unit Test', function() {
   const invalidUser = {
     'username': 'dave',
   };
-  it('Test 13: error if missing field when creating user', function(done) {
+  it('Test 19: error if missing field when creating user', function(done) {
     authenticatedUser
         .post('/create/user')
         .set('Content-Type', 'application/json')
@@ -203,7 +276,7 @@ describe('User REST API Unit Test', function() {
   const invalidUpdate = {
     'username': 'dave',
   };
-  it('Test 14: error if update field is missing', function(done) {
+  it('Test 20: error if update field is missing', function(done) {
     authenticatedUser
         .post('/update/user')
         .set('Content-Type', 'application/json')
@@ -214,7 +287,7 @@ describe('User REST API Unit Test', function() {
         });
   });
 
-  it('Test 15: delete current session', function(done) {
+  it('Test 21: delete current session', function(done) {
     authenticatedUser
         .delete('/delete/session')
         .set('Content-Type', 'application/json')
