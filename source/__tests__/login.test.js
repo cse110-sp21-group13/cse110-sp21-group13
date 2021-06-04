75d282cf1fefbff71d3a96a180bae8a51d3821aa
Zhongqian <zhl024@ucsd.edu>
2021-06-04 12:12:45 -0500

    lint fix

--- source/__tests__/login.test.js
+++ source/__tests__/login.test.js
@@ -5,8 +5,8 @@ let browser;
 describe('Basic user flow for login page', () => {
   beforeAll(async () => {
     browser = await puppeteer.launch({headless: true,
-      slowMo: 100, testTimeout: 50000, 
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],});
+      slowMo: 100, testTimeout: 50000,
+      args: ['--no-sandbox', '--disable-setuid-sandbox']});
     page = await browser.newPage();
     await page.goto('https://journalbullet.herokuapp.com');
   });
